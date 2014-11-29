51c26eb90329e730eedb625ba49776fd996cee6c
betza <betzabe.jacob@gmail.com>
2014-11-29 11:24:57 -0800

    Actualizacion

--- src/dream/aplicacion/Inicio.java
+++ src/dream/aplicacion/Inicio.java
@@ -3,10 +3,13 @@ package dream.aplicacion;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import Entidad.Usuario;
+
 @ManagedBean
 @ViewScoped
 public class Inicio {
 
+	private Usuario usuario;
 	public void doActionHello(){
 		System.out.println("Ouh yeah!");
 	}
